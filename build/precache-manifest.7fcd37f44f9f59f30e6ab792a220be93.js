32ad3c5c2508febdb801cc715739600565ec81a3
Farrukh <48393416+faralummers@users.noreply.github.com>
2020-05-10 18:10:10 +0200

    Created dist folder

---
+++ build/precache-manifest.7fcd37f44f9f59f30e6ab792a220be93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7278b530d4f0708fed91440d8a51a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2de81b88099f1056ae6",
+    "url": "/static/css/main.4532288e.chunk.css"
+  },
+  {
+    "revision": "8c8be03abacfdda0a87a",
+    "url": "/static/js/2.f899221b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f899221b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2de81b88099f1056ae6",
+    "url": "/static/js/main.037a7e6e.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
